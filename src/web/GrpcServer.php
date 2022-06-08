e8753c336299ad1a8f34d74fa1fc0b073d5ca9a0
Mikhail B <spywer@mail.ru>
2022-06-08 09:54:44 +0300

    Add GRPC Server

---
+++ src/web/GrpcServer.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace swoole\foundation\web;
+
+use Throwable;
+use Yii;
+use yii\base\InvalidConfigException;
+
+class GrpcServer extends Server
+{
+    /**
+     * @var string 监听主机
+     */
+    public $host = 'localhost';
+    /**
+     * @var int 监听端口
+     */
+    public $port = 50051;
+    /**
+     * @var int 进程模型
+     */
+    public $mode = SWOOLE_PROCESS;
+    /**
+     * @var int SOCKET类型
+     */
+    public $sockType = SWOOLE_SOCK_TCP;
+    /**
+     * @var array 服务器选项
+     */
+    public $options = [
+        'worker_num' => 1,
+        'daemonize' => 0,
+        'task_worker_num' => 1,
+        'open_http2_protocol' => true
+    ];
+    /**
+     * @var array 应用配置
+     */
+    public $app = [];
+    /**
+     * @var \Swoole\Http\Server swoole server实例
+     */
+    public $server;
+
+    /**
+     * @inheritDoc
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty($this->app)) {
+            throw new InvalidConfigException('The GRPC "app" property mus be set.');
+        }
+
+        if (!$this->server instanceof \Swoole\Http\Server) {
+            $this->server = new \Swoole\Http\Server($this->host, $this->port, $this->mode, $this->sockType);
+            $this->server->set($this->options);
+        }
+
+        foreach ($this->events() as $event => $callback) {
+            $this->server->on($event, $callback);
+        }
+    }
+
+    /**
+     * 事件监听
+     * @return array
+     */
+    public function events()
+    {
+        return [
+            'start' => [$this, 'onStart'],
+            'workerStart' => [$this, 'onWorkerStart'],
+            'workerError' => [$this, 'onWorkerError'],
+            'workerStop' => [$this, 'onWorkerStop'],
+            'workerExit' => [$this, 'onWorkerExit'],
+            'request' => [$this, 'onRequest'],
+            'task' => [$this, 'onTask'],
+            'managerStart' => [$this, 'onManagerStart'],
+            'finish' => [$this, 'onFinish']
+        ];
+    }
+
+    /**
+     * 启动服务器
+     * @return bool
+     */
+    public function start()
+    {
+		swoole_set_process_name("Yii2 GRPC Server: master");
+		
+        return $this->server->start();
+    }
+	
+	public function onManagerStart()
+    {
+		swoole_set_process_name("Yii2 GRPC Server: manager");
+    }
+
+    /**
+     * master启动
+     * @param \Swoole\Http\Server $server
+     */
+    public function onStart(\Swoole\Http\Server $server)
+    {
+        printf("GRPC listen on %s:%d\n", $server->host, $server->port);
+    }
+
+    /**
+     * 工作进程启动时实例化框架
+     * @param \Swoole\Http\Server $server
+     * @param int $workerId
+     * @throws InvalidConfigException
+     */
+    public function onWorkerStart(\Swoole\Http\Server $server, $workerId)
+    {
+        new Application($this->app);
+		
+        Yii::$app->set('server', $server);
+		
+		$this->processRename($server, $workerId);
+		
+		echo "GRPC Worker# $workerId started" . PHP_EOL;
+		
+		if(Yii::$app->hasModule('tasks') && !$server->taskworker && $server->setting['worker_num'] >= 1 && $server->getWorkerId() == 0) {
+			Yii::$app->getModule('tasks')->start();
+		}
+    }
+
+    /**
+     * 工作进程异常
+     * @param \Swoole\Http\Server $server
+     * @param $workerId
+     * @param $workerPid
+     * @param $exitCode
+     * @param $signal
+     */
+    public function onWorkerError(\Swoole\Http\Server $server, $workerId, $workerPid, $exitCode, $signal)
+    {
+        fprintf(STDERR, "GRPC worker error. id=%d pid=%d code=%d signal=%d\n", $workerId, $workerPid, $exitCode, $signal);
+    }
+
+    /**
+     * 处理请求
+     * @param \Swoole\Http\Request $request
+     * @param \Swoole\Http\Response $response
+     */
+    public function onRequest(\Swoole\Http\Request $request, \Swoole\Http\Response $response)
+    {
+        Yii::$app->request->setRequest($request);
+        Yii::$app->response->setResponse($response);
+        Yii::$app->run();
+        Yii::$app->response->clear();
+    }
+
+    /**
+     * 分发任务
+     * @param \Swoole\Http\Server $server
+     * @param $taskId
+     * @param $workerId
+     * @param $data
+     * @return mixed
+     */
+    public function onTask(\Swoole\Http\Server $server, $taskId, $workerId, $data)
+    {
+        try {
+            $handler = $data[0];
+            $params = $data[1] ?? [];
+            list($class, $action) = $handler;
+
+            $obj = new $class();
+            return call_user_func_array([$obj, $action], $params);
+        } catch (Throwable $e) {
+            Yii::$app->errorHandler->handleException($e);
+            return 1;
+        }
+    }
+	
+	public function onWorkerStop(\Swoole\Http\Server $server, $workerId)
+    {
+        echo "GRPC Worker# $workerId stopped" . PHP_EOL;
+    }
+	
+	public function onWorkerExit(\Swoole\Http\Server $server, $workerId)
+    {
+        echo "GRPC Worker# $workerId exit" . PHP_EOL;
+    }
+
+	public function onFinish(\Swoole\Http\Server $server)
+    {
+		echo "GRPC Task finished" . PHP_EOL;
+	}
+	
+	protected function processRename(\Swoole\Http\Server $server, $worker_id) {
+
+		if ($server->taskworker) {
+			swoole_set_process_name("Yii2 GRPC Server: task");
+		} else {
+			swoole_set_process_name("Yii2 GRPC Server: worker");
+		}
+	
+	}
+
+}
